17670ca172522eee265b34fd9d5888d3c0c89a3c
linnnnnnn <linchengxianfr@gmail.com>
2018-02-07 02:00:36 +0100

    page commande
    1. passer en php
    2. connexion au BD

---
+++ php/commande.php
@@ -0,0 +1,3 @@
+<?php
+
+require('connectDB.php');
--- php/test.php
+++ php/test.php
@@ -1,12 +1,35 @@
 <?php
-//  启动会话，这步必不可少
-session_start();
-//  判断是否登陆
-if (isset($_SESSION["username"]) && $_SESSION["logged"] === true) {
-    echo "您已经成功登陆".$_SESSION["username"];
-} else {
-    //  验证失败，将 $_SESSION["admin"] 置为 false
-    $_SESSION["logged"] = false;
-    die("您无权访问");
+require('connectDB.php');
+$books = array();
+$i = 0;
+$res = $dbh->query( "SELECT * FROM `ouvrages`");
+
+$res_all = $res->fetchAll(PDO::FETCH_ASSOC);
+foreach ($res_all as $row){
+    $books[$i]['idouvrage'] = $row['idouvrage'];
+    $books[$i]['titre'] = $row['titre'];
+    $books[$i]['auteur'] = $row['auteur'];
+    $books[$i]['prix'] = $row['prix'];
+    $i++;
 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+</head>
+<body>
+
+<?php
+echo "<table>";
+foreach ($books as $value) {
+    echo "<tr><td>" . $value['titre'] . "</td><td>" . $value['auteur'] . "</td><td>" . $value['prix']
+        . "<td class=\"quantiteLivre\"><input name=\"cyrano\" type=\"number\" value=\"0\" min=\"0\"></input></td>"
+        . "</td></tr>";
+}
+echo "</table>";
+
+?>
+<a href="">Passer commande</a>
+</body>
+</html>
